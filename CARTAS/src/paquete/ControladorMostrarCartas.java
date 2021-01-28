dd562158db15080bb3ade8ba6ed0508eb704c0a6
Jesus <jsr7manzanares@gmail.com>
2021-01-28 12:14:15 +0100

    jejeje

---
+++ CARTAS/src/paquete/ControladorMostrarCartas.java
@@ -0,0 +1,38 @@
+package paquete;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class ControladorMostrarCartas {
+
+	@FXML
+	Button btnStreamers;
+	@FXML
+	Button btnDibujos;
+	@FXML
+	Button btnIconos;
+	@FXML
+	Button btnCsgo;
+	
+	@FXML
+	public void Streamers(ActionEvent event) {
+		
+	}
+	
+	public void btnDibujos(ActionEvent event) {
+		
+	}
+
+	public void btnIconos(ActionEvent event) {
+	
+	}
+	
+	public void btnCsgo(ActionEvent event) {
+		
+	}
+}
--- CARTAS/src/paquete/Controller.java
+++ CARTAS/src/paquete/Controller.java
@@ -23,44 +23,8 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
+public class Controller{
 
-    @FXML
-    RadioButton r1;
-    @FXML
-    RadioButton r2;
-    @FXML
-    RadioButton r3;
-    @FXML
-    RadioButton r4;
-    @FXML
-    RadioButton r5;
-    @FXML
-    RadioButton r6;
-    @FXML
-    RadioButton r7;
-    @FXML
-    RadioButton r8;
-    @FXML
-    Button btnAtacar;
-    @FXML
-    ToggleGroup grupo1;
-    @FXML
-    Button btnCarta1;
-    @FXML
-    Button btnCarta2;
-    @FXML
-    Button btnCarta3;
-    @FXML
-    Button btnCarta4;
-    @FXML
-    Button btnCarta5;
-    @FXML
-    Button btnCarta6;
-    @FXML
-    Button btnCarta7;
-    @FXML
-    Button btnCarta8;
     @FXML 
     Button btnVolumen;
     @FXML
@@ -69,7 +33,7 @@ public class Controller implements Initializable {
     Button btnJugar;
     
    
-    public void initialize(URL location, ResourceBundle resources) {
+    public void initialize() {
     	audio.play();
     	URL url = getClass().getResource("/img/altavoz.png");
     	Image imagenAltavoz = new Image(url.toString(),70,80,false,true);
@@ -92,106 +56,31 @@ public class Controller implements Initializable {
     	
     }
 
-    public void click1(MouseEvent event){
-
-    if(r1.isSelected()){
-        r1.setSelected(false);
-    }else{
-        r1.setSelected(true);
-    }
-    turno();
-    }
-
-    public void click2(MouseEvent event){
-
-        if(r2.isSelected()){
-            r2.setSelected(false);
-        }else{
-            r2.setSelected(true);
-        }
-        turno();
-    }
-
-    public void click3(MouseEvent event){
-
-        if(r3.isSelected()){
-            r3.setSelected(false);
-        }else{
-            r3.setSelected(true);
-        }
-        turno();
-    }
-    public void click4(MouseEvent event){
-
-        if(r4.isSelected()){
-            r4.setSelected(false);
-        }else{
-            r4.setSelected(true);
-        }
-        turno();
-    }
-    public void click5(MouseEvent event){
-
-        if(r5.isSelected()){
-            r5.setSelected(false);
-        }else{
-            r5.setSelected(true);
-        }
-        turno();
-    }
-    public void click6(MouseEvent event){
-
-        if(r6.isSelected()){
-            r6.setSelected(false);
-        }else{
-            r6.setSelected(true);
-        }
-        turno();
-    }
-    public void click7(MouseEvent event){
-
-        if(r7.isSelected()){
-            r7.setSelected(false);
-        }else{
-            r7.setSelected(true);
-        }
-        turno();
-    }
-
-    public void click8(MouseEvent event){
-
-        if(r8.isSelected()){
-            r8.setSelected(false);
-        }else{
-            r8.setSelected(true);
-        }
-        turno();
-    }
-
-
-
-    public void turno(){
-        if ((r1.isSelected()|r2.isSelected()|r3.isSelected()|r4.isSelected()&& (r5.isSelected()|r6.isSelected()|r7.isSelected()|r8.isSelected()))){
-            btnAtacar.setDisable(false);
-        }else{
-            btnAtacar.setDisable(true);
-        }
-    }
     
-    @FXML
-    public void btnJugar(ActionEvent event) throws Exception {
-    	Stage primaryStage=new Stage();
-		((Node) event.getSource()).getScene().getWindow().hide();
-		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Partida.fxml"));
-			Scene scene = new Scene(root,900,670);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+    
+    public void mostrarCartas(ActionEvent event) {
+    	try {
+			Stage primaryStage= new Stage();
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("MostrarCartas.fxml"));
+			Scene scene = new Scene(root,1300,830);
 			primaryStage.setScene(scene);
 			primaryStage.setResizable(false);
 			primaryStage.show();
-		} catch(Exception v) {
-			v.printStackTrace();
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
-	    
+    }
+    
+    public void jugar(ActionEvent event) {
+    	try {
+			Stage primaryStage= new Stage();
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Partida.fxml"));
+			Scene scene = new Scene(root,1300,830);
+			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
     }
 }
